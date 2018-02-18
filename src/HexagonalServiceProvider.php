6390a8aafaab834c8614bee69b2b69cb976178bb
Boudy de Geer <boudydegeer@mosaiqo.com>
2018-02-18 23:22:13 +0100

    Add more commands

--- src/HexagonalServiceProvider.php
+++ src/HexagonalServiceProvider.php
@@ -22,6 +22,9 @@ class HexagonalServiceProvider extends ServiceProvider {
 		$this->commands([
 			\Mosaiqo\Hexagonal\Console\Commands\ChangeSourceNamespaceCommand::class,
 			\Mosaiqo\Hexagonal\Console\Commands\MakeServiceCommand::class,
+			\Mosaiqo\Hexagonal\Console\Commands\MakeFeatureCommand::class,
+			\Mosaiqo\Hexagonal\Console\Commands\MakeControllerCommand::class,
+			\Mosaiqo\Hexagonal\Console\Commands\ListFeaturesCommand::class,
 		]);
 	}
 
